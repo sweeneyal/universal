568597d783305aa640b638f33d33d14bd47285fb
Andrew Sweeney <sweeneyandrewliam@gmail.com>
2023-09-14 22:36:13 -0500

    Adding character and byte support

--- TypeUtilityPkg.vhd
+++ TypeUtilityPkg.vhd
@@ -3,9 +3,14 @@ library ieee;
     use ieee.numeric_std.all;
 
 package TypeUtilityPkg is
+
+    subtype byte is std_logic_vector(7 downto 0);
     
     function ToStdLogicVector(intVal : integer; size : natural) return std_logic_vector;
     function ToInteger(vector : std_logic_vector) return integer;
+    function CheckStringEqual(str1 : string; str2 : string) return boolean;
+    function ToByte(char : character) return byte;
+    function ToCharacter(by : byte) return character;
     
 end package TypeUtilityPkg;
 
@@ -19,6 +24,25 @@ package body TypeUtilityPkg is
     function ToInteger(vector : std_logic_vector) return integer is
     begin
         return to_integer(unsigned(vector));
+    end function;
+
+    function CheckStringEqual(str1 : string; str2 : string) return boolean is
+    begin
+        if str1'length /= str2'length then
+            return false;
+        else
+            return (str1 = str2);
+        end if;
+    end function;
+
+    function ToByte(char : character) return byte is
+    begin
+        return byte(to_unsigned(character'pos(char), 8));
+    end function;
+
+    function ToCharacter(by : byte) return character is
+    begin
+        return character'val(to_integer(unsigned(by)));
     end function;
     
 end package body TypeUtilityPkg;
