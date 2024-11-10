b8acedc54e67a401d9492cef21c710f9b000e5a4
Andrew Sweeney <sweeneyandrewliam@gmail.com>
2024-11-10 13:44:05 -0600

    Fixing tcl script

--- tcl/universal.tcl
+++ tcl/universal.tcl
@@ -17,7 +17,7 @@ proc universal::autoinit {} {
 # Get the repository path of the IP
 proc universal::get_ip_repo {} {
     variable tcldir
-    return [file dirname $tcldir/..]
+    return [file dirname $tcldir/../..]
 }
 
 # This is a script that will read all of this IP's internal source files.
@@ -26,8 +26,8 @@ proc universal::read_library_sources {} {
 
     # Get all package files
     add_files $home/hdl/pkgs/CommonFunctions.vhd
-    set_property library rktcpu [get_files CommonFunctions.vhd]
+    set_property library universal [get_files CommonFunctions.vhd]
 
     add_files $home/hdl/pkgs/CommonTypes.vhd
-    set_property library rktcpu [get_files CommonTypes.vhd]
+    set_property library universal [get_files CommonTypes.vhd]
 }
